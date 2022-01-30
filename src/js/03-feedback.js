f130b52860e4a0c468279363c8c18b6f5e527aad
Alex <41620816+PhazZzyo@users.noreply.github.com>
2022-01-30 16:56:18 +0200

    Update 03-feedback.js
    Task 03 complete

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
+const form = document.querySelector('form');
+const emailField = document.querySelector('[name="email"]');
+const messageField = document.querySelector('[name="message"]');
+const savedData = localStorage.getItem('feedback-form-state');
+const parsedSavedData = JSON.parse(savedData);
 
+form.addEventListener('input', throttle(handleInput, 500));
+form.addEventListener('submit', handleSubmit);
+
+if (parsedSavedData) {
+  emailField.value = parsedSavedData.email;
+  messageField.value = parsedSavedData.message;
+}
+
+function handleInput(event) {
+  const email = form.email.value;
+  const message = form.message.value;
+
+  console.log(`Email: ${email}, message: ${message}`);
+
+  localStorage.setItem('feedback-form-state', JSON.stringify({ email, message }));
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+
+  if (email.value === '' || message.value === '') {
+    return alert('Please fill in all the fields!');
+  }
+
+  console.log(`Email: ${email.value}, message: ${message.value}`);
+  event.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
