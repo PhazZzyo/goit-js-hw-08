56c8be8cdfe35f2d4a06d09b3ed8bd45df2e49ea
Alex <41620816+PhazZzyo@users.noreply.github.com>
2022-01-30 15:31:40 +0200

    2nd task
    Task 02 is copleted

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,36 +2,23 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
+const savedTime = localStorage.getItem('videoplayer-current-time');
+const parsedSavedTime = JSON.parse(savedTime);
 
-const savedTime = (data) => {
-    localStorage.setItem('videoplayer-current-time', JSON.stringify(player.on(data)))
-    console.log(savedTime);
+const onPlay = data => {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(data.seconds));
 };
-    
 
-player.on('timeupdate', throttle(savedTime, 1000));
+player.on('timeupdate', throttle(onPlay, 1000));
 
+player.setCurrentTime(parsedSavedTime).catch(function (error) {
+  switch (error.name) {
+    case 'TypeError':
+      // the id was not a number
+      break;
 
-    // player.getVideoTitle().then(function(title) {
-    //     console.log('title:', title);
-    // });
-
-// player.setCurrentTime(30.456).then(function(seconds) {
-//     // seconds = the actual time that the player seeked to
-// }).catch(function(error) {
-//     switch (error.name) {
-//         case 'RangeError':
-//             // the time was less than 0 or greater than the video’s duration
-//             break;
-
-//         default:
-//             // some other error occurred
-//             break;
-//     }
-// });
-
-// localStorage.setItem('videoplayer-current-time', JSON.stringify(player.getCurrentTime(seconds)));
-
-
-// const parsedTime = JSON.parse(savedTime);
-// console.log(savedTime, parsedTime);
+    default:
+      // some other error occurred
+      break;
+  }
+});
